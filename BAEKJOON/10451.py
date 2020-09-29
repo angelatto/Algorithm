3a979149d29746d4c11db950fcd3800a9e029d6a
angelatto <pudri109@gmail.com>
2020-09-29 12:42:35 +0900

    backjoon 10451.py solve

---
+++ BAEKJOON/10451.py
@@ -0,0 +1,34 @@
+#아이디어
+# 1. 일단 그래프를 만든다.
+# 2. 그래프를 탐색하여 사이클을 찾으면 +1을 한다.
+# if __name__ == "__main__":
+import sys
+
+testcase = int(input())
+for _ in range(testcase): #2개
+    n = int(input()) #8개
+
+    def get_cnt():
+        cnt = 0
+        # idx는 인덱스인데 인덱스로 보고 1-n까지 체크할꺼라서
+        sequential = [0] + list(map(int, sys.stdin.readline().split()))
+        visit  = [0] * (n + 1) #방문 여부 확인
+
+        # idx:          1 2 3 4 5 6 7 8
+        # sequential: 0 3 2 7 8 1 4 5 6
+        # visit:      0 1 1 1 0 1 0 1 0
+
+        for idx in range(1, n+1): #1~n까지
+            i = idx
+            if visit[i] == 0: #처음 방문한 것이면
+                cnt += 1
+            else:
+                continue
+
+            while not visit[i]: #방문안했다면...사이클 최대한 돌아서 방문표시하기
+                visit[i] = 1
+                i = sequential[i]
+
+        return cnt
+
+    print(get_cnt())
