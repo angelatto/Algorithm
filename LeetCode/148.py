63c7c598f8d8d2c0c8f43304340b7683827adde5
angelatto <pudri109@gmail.com>
2020-10-07 15:19:14 +0900

    LeetCode 148 study

---
+++ LeetCode/148.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sortList(self, head: ListNode) -> ListNode:
+        # 연결리스트 -> 파이썬 리스트
+        p = head
+        lst = []
+        while p:
+            lst.append(p.val)
+            p = p.next
+
+        # 정렬
+        lst.sort()
+
+        #파이썬 리스트 -> 연결 리스트
+        p = head
+        for i in range(len(lst)):
+            p.val = lst[i]
+            p = p.next
+        return head
