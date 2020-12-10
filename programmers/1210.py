958110e00bf546e1a9efaa5cec0adb70d329e49d
angelatto <pudri109@gmail.com>
2020-12-10 22:56:21 +0900

    programmers 1210.py lev.5 ing

--- programmers/1210.py
+++ programmers/1210.py
@@ -15,37 +15,68 @@ def solution(arrows):
         after = (curX, curY)
         # print('before: ', before, 'after: ', after)
 
-        graph[before].append(after)
-        graph[after].append(before)  # 양방향이라서 이렇게 함
-
+        graph[before].append(after) # 헐 사실은 이거 단방향임
+        # graph[after].append(before)  # 양방향이라서 이렇게 함
         before = after
     # print(graph)
 
-    # 2. 그래프(인접리스트) DFS 탐색하면서 사이클 찾아내기
-    traced = set()
-    # 양방향 그래프라서 자기 자신으로 돌아오지 않게 해야함
-    # 양방향이라서 방문체크가 필요함.....
-    # 백트래킹할때를 카운트하면 어떨지....???/?
-    # 백트링하는 시점을 어디서 출력해야 할지 고민해보기 
-    # visited = []
+    # # 2. 그래프(인접리스트) DFS 탐색하면서 사이클 찾아내기
+    traced = []
 
-    def dfs(node): #사이클이 있으면 재귀 종료가 안됨.
-        print('node: ', node)
-        if node in traced:
+    # # 양방향 그래프라면 자기 자신으로 돌아오지 않게 해야함
+    # # 양방향이라면 방문체크가 필요함.....
+    # 하지만 이건 단방향이니까 이런게 필요없는 거임
+
+    def dfs(node):
+        global answer
+        # print('node: ', node)
+
+        if len(traced) != 0 and traced[-1][0] != node[0] and traced[-1][1] != node[1]: #대각선
+            nx = (traced[-1][0]+node[0])/2
+            ny = (traced[-1][1]+node[1])/2
+            if (nx, ny) in traced:
+                traced.append((nx, ny))
+                answer += 1
+            if node in traced:
+                traced.append(node)
+                answer += 1
+            print(traced)
+
+            if (nx, ny) in traced or node in traced:
+                return False
+
+        elif node in traced:  # 대각선 아닐때
+            traced.append(node)
+            print(traced)
+            answer += 1
             return False
 
-        traced.add(node)
+
+        if len(traced) != 0 and traced[-1][0] != node[0] and traced[-1][1] != node[1]:
+            nx = (traced[-1][0]+node[0])/2
+            ny = (traced[-1][1]+node[1])/2
+            traced.append((nx, ny))
+        traced.append(node)
+        print('중복이 없을 때: ', traced)
+
         for w in graph[node]:
             if not dfs(w):
                 return False
+
         return True
 
     #--main--
+    global answer
+    answer = 0
     start = (0, 0)
     for x in list(graph):
-        if not dfs(x):
-            print('----')
+        if x not in traced:
+            if not dfs(x):
+                print('cycle')
 
-    return 0
+    return answer
 
-print(solution([6, 6, 6, 4, 4, 4, 2, 2, 2, 0, 0, 0, 1, 6, 5, 5, 3, 6, 0]))
+# print(solution([6, 6, 6, 4, 4, 4, 2, 2, 2, 0, 0, 0, 1, 6, 5, 5, 3, 6, 0]))
+# print(solution([6, 5, 2, 7, 1, 4, 2, 4, 6]))
+# print(solution([5, 2, 7, 1, 6, 3]))
+print(solution([6, 2, 4, 0, 5, 0, 6, 4, 2, 4, 2, 0]))
