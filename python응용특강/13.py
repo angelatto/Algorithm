1f98ba946da255a58b83d0083064d5102864c713
angelatto <pudri109@gmail.com>
2021-01-21 17:00:06 +0900

    git commit -m "Python turtle module 13.py"

---
+++ python응용특강/13.py
@@ -0,0 +1,12 @@
+import turtle
+
+t = turtle.Turtle()
+t.shape('turtle')
+t.shapesize(6)  # 거북이 사이즈를 확대
+# t.shapesize(2, 1)
+t.color('blue')
+t.color(0, 0, 0)  # r, g, b
+t.fd(100)
+t.color(1, 1, 1)
+
+turtle.mainloop()
