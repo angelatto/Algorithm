05528481b87c7fd540967a0ca3d1b448f8ab9eec
angelatto <pudri109@gmail.com>
2021-01-21 17:00:27 +0900

    git commit -m "Python turtle module 17.py"

---
+++ python응용특강/17.py
@@ -0,0 +1,19 @@
+import turtle
+
+t = turtle.Turtle()
+t.shape('turtle')
+t.shapesize(4)
+
+t.color('black', 'red')
+t.stamp()
+t.fd(100)
+
+t.color('green', 'yellow')
+t.stamp()
+t.fd(100)
+
+t.color('blue', 'orange')
+t.stamp()
+
+
+turtle.mainloop()
