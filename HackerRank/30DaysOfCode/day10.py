f1176f0e07445931464a22e57d89f5c786706eb2
angelatto <pudri109@gmail.com>
2021-01-25 19:32:06 +0900

    git commit -m "HackerRank day10 solve"

---
+++ HackerRank/30DaysOfCode/day10.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input())
+    num = bin(n)
+
+    maxlen = 0
+    temp = 0
+    for i in range(len(num)):
+        if num[i] == '1':
+            temp += 1
+            maxlen = max(maxlen, temp)
+        else:
+            temp = 0
+    print(maxlen)
