61280cc1d5e1fafcd92e6f24d42dffd0d07311d6
angelatto <pudri109@gmail.com>
2021-01-26 16:59:10 +0900

    git commit -m "BaekJoon 2133.py solve (dp) add desc"

--- BAEKJOON/dynamic-programming/2133.py
+++ BAEKJOON/dynamic-programming/2133.py
@@ -1,7 +1,8 @@
 """
 타일 채우기 (lev.silver-1)
 -다이나믹 프로그래밍
-높이 3, 가로 길이 N인 직사각형 경우의 수
+점화식 설명
+: 길이가 N인 자기 자신 2개 + 길이가 2인거랑 * 길이가 N-2인거랑 + 나머지꺼 각각 * 2(더 이상 쪼개지지 않는)
 """
 import collections
 
@@ -10,6 +11,6 @@ dp = collections.defaultdict(int)
 dp[2] = 3
 
 for i in range(4, N+1, 2):
-    dp[i] = 2 + dp[i-2] * 3 + (sum(dp.values()) - dp[i-2]) * 2
+    dp[i] = 2 + dp[i-2] * dp[2] + (sum(dp.values()) - dp[i-2]) * 2
 
 print(dp[N])
