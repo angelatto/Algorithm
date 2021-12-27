b39bc959bf222c4057c6a62ea3a72ad3315985de
angelatto <pudri109@gmail.com>
2021-12-27 17:32:24 +0900

    파이썬 부수기 완료

---
+++ study_algo/ch01/6.py
@@ -0,0 +1,18 @@
+
+# 람다 표현식 예시 1
+array = [('홍길동', 50), ('이순신', 32), ('아무개', 74)]
+
+def my_key(x):
+    return x[1]
+
+print(sorted(array, key=my_key))
+print(sorted(array, key=lambda x: x[1]))
+
+
+# 람다 표현식 예시 2
+list1 = [1, 2, 3, 4, 5]
+list2 = [6, 7, 8, 9, 10]
+
+result = map(lambda a, b: a + b, list1, list2)
+
+print(list(result))
