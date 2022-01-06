494fb189b106e52971da35cc3dcf67b05fb179de
angelatto <pudri109@gmail.com>
2022-01-06 17:53:05 +0900

    백준 10866 solved

---
+++ BAEKJOON/1일1솔/10866.py
@@ -0,0 +1,58 @@
+"""
+01.06.22
+실버4
+
+push_front X: 정수 X를 덱의 앞에 넣는다.
+push_back X: 정수 X를 덱의 뒤에 넣는다.
+pop_front: 덱의 가장 앞에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+pop_back: 덱의 가장 뒤에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+size: 덱에 들어있는 정수의 개수를 출력한다.
+empty: 덱이 비어있으면 1을, 아니면 0을 출력한다.
+front: 덱의 가장 앞에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+back: 덱의 가장 뒤에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+
+"""
+
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline())
+q = deque()
+
+for _ in range(N):
+    li = sys.stdin.readline().split()
+    cmd = li[0]
+    if len(li) > 1:
+        num = li[1]
+
+    if cmd == 'push_front':
+        q.appendleft(num)
+    elif cmd == 'push_back':
+        q.append(num)
+    elif cmd == 'pop_front':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q.popleft())
+    elif cmd == 'pop_back':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q.pop())
+    elif cmd == 'size':
+            print(len(q))
+    elif cmd == 'empty':
+        if len(q) > 0:
+            print(0)
+        else:
+            print(1)
+    elif cmd == 'front':
+        if len(q) > 0:
+            print(q[0])
+        else:
+            print(-1)
+    elif cmd == 'back':
+        if len(q) > 0:
+            print(q[-1])
+        else:
+            print(-1)
